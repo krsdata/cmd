182ce4c14891f575ae5541e436201ee1c97d690e
krsdata <49938458+krsdata@users.noreply.github.com>
2019-10-17 00:53:14 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,1 +1,11 @@
-sudo apt install phpmyadmin
+sudo apt update && updgrade
+sudo apt install curl zip git -y
+sudo apt install mysql-client mysql-server
+sudo apt install phpmyadmin composer -y
+sudo a2enmod rewrite
+sudo   systemctl restart apache2
+sudo useradd developer
+sudo usermod -d /var/www developer
+sudo chown -R developer:root /var/www
+sudo find /etc/ssh/ -name "sshd_config"  -exec sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" {} \;
+sudo /etc/init.d/ssh restart
