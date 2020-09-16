00353fd3990e5da66c5cb6283125f2da30b85f0f
krsdata <49938458+krsdata@users.noreply.github.com>
2020-09-16 19:20:59 +0530

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -3,7 +3,7 @@ sudo apt install curl zip git -y
 apt-get install software-properties-common
 add-apt-repository ppa:ondrej/php -y
 sudo apt install php composer -y
-sudo apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml libapache2-mod-php -y
+sudo apt-get install php-pear php-fpm php-dev php-zip php-curl php-xmlrpc php-gd php-mysql php-mbstring php-xml php-gettext libapache2-mod-php -y
 sudo apt install mysql-client mysql-server -y
 sudo a2enmod rewrite
 sudo   systemctl restart apache2
@@ -12,6 +12,6 @@ sudo usermod -d /var/www developer
 sudo chown -R developer:root /var/www
 sudo find /etc/ssh/ -name "sshd_config"  -exec sed -i "s/PasswordAuthentication no/PasswordAuthentication yes/g" {} \;
 sudo /etc/init.d/ssh restart
-sudo apt install phpmyadmin php-mbstring php-gettext -y
-sudo phpenmod mbstring
-sudo apt update && updgrade
+sudo apt install phpmyadmin  -y
+sudo apt update && updgrade -y
+sudo apt autoremove
